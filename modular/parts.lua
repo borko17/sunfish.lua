35ad1816e54ae75832fd03b9a8de6f56111ab517
Borko Danilović <borko.danilovic17@gmail.com>
2026-08-28 07:11:16 +0200

    Create parts.lua

---
+++ modular/parts.lua
@@ -0,0 +1,6 @@
+-- parts.lua =======
+-- List of files loader.lua downloads and runs, in order (after this file
+-- itself). Add/remove/reorder module files here only - loader.lua never
+-- needs to change.
+PARTS = {"core.lua", "search.lua", "ui.lua", "help.lua", "mate1.lua", "challenge.lua", "debug.lua", "debug2.lua", "main.lua"}
+-- parts.lua ======= end
