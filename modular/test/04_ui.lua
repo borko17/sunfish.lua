e2462abc7f6de55eb4f094d939a708ce0402f137
Borko Danilović <borko.danilovic17@gmail.com>
2026-09-06 13:08:40 +0200

    Update 04_ui.lua

--- modular/test/04_ui.lua
+++ modular/test/04_ui.lua
@@ -146,11 +146,11 @@ function printEngineScore()
    end
 
    if score < 0 then
-      echoS(string.format("Score: +%d (You)", math.abs(score)))
+      echoS(string.format("   Score: +%d (You)", math.abs(score)))
    elseif score > 0 then
-      echoE(string.format("Score: +%d (Sunfish)", score))
+      echoE(string.format("   Score: +%d (Sunfish)", score))
    else
-      echoW("Score: 0 (equal)")
+      echoW("   Score: 0 (equal)")
    end
 end
 
