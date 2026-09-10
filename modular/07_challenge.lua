591a3cf18239e23112fc35d63c4cf6c6cf9b539d
Borko Danilović <borko.danilovic17@gmail.com>
2026-09-10 13:41:00 +0200

    Update 07_challenge.lua

--- modular/07_challenge.lua
+++ modular/07_challenge.lua
@@ -247,6 +247,9 @@ function playChallengeGame(board, startPos, startLastMove, startCapturedByUser,
       local usermove = nil
       while true do
          print("Your ".. (whiteMoves + 1) ..". move: ")
+         local startInputTime = os.clock()  -- start timing
+   local crdn = input()
+   local inputElapsed = os.clock() - startInputTime  -- elapsed time
          local crdn = input()
          if not crdn then
             echoE("\nNo input (EOF). Ending challenge.")
